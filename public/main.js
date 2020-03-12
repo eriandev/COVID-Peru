2eb898aba4cab512450642bd35c65b58110e5a9b
erianvc <erianvc.dev@gmail.com>
2020-03-12 17:40:14 -0500

    Casos locales se actualizan mediante una API

--- public/main.js
+++ public/main.js
@@ -1,10 +1,12 @@
-const API = 'https://erianvc.github.io/api/COVID-Peru/worldCases.json'
+const API = 'https://erianvc.github.io/api/COVID-Peru/'
 
 const worldInfected = document.getElementById('world-infected')
 const worldRecovered = document.getElementById('world-recovered')
 const worldDeaths = document.getElementById('world-deaths')
 
-let map = L.map('map').setView([-9,-75.0282], 5)
+const localInfected = document.getElementById('local-infected')
+const localRecovered = document.getElementById('local-recovered')
+const localDeaths = document.getElementById('local-deaths')
 
 const circleMarkerOpts = {
     radius: 6,
@@ -15,26 +17,51 @@ const circleMarkerOpts = {
     fillOpacity: 0.6
 }
 
-updateWorldCases()
+let map = L.map('map').setView([-9,-75.0282], 5)
 
 L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
     maxZoom: 20,
     attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-L.circleMarker([-12.022867,-77.110848], circleMarkerOpts).addTo(map).bindPopup('16 casos confirmados en Lima')
-L.circleMarker([-9.926930,-76.242220], circleMarkerOpts).addTo(map).bindPopup('2 casos confirmados en Huánuco')
-L.circleMarker([-13.4179319,-76.1396996], circleMarkerOpts).addTo(map).bindPopup('2 casos confirmados en Chincha')
-L.circleMarker([-16.342461,-71.569104], circleMarkerOpts).addTo(map).bindPopup('2 casos confirmados en Arequipa')
 
-async function getWorldCases(){
-    return await fetch(API).then(res => res.json())
+
+init()
+
+
+
+function init(){
+    
+    updateLocalCases()
+    updateWorldCases()
+}
+
+async function getData(URL){
+
+    return await fetch(URL).then(res => res.json())
 }
 
 async function updateWorldCases(){
-    let data = await getWorldCases()
-    console.log(data)
+
+    const data = await getData(`${API}worldCases.json`)
     worldInfected.textContent = new Intl.NumberFormat().format(data.cases).replace(/\./g,' ')
     worldRecovered.textContent = new Intl.NumberFormat().format(data.recovered).replace(/\./g,' ')
     worldDeaths.textContent = new Intl.NumberFormat().format(data.deaths).replace(/\./g,' ')
+}
+
+async function updateLocalCases(){
+
+    const data = await getData(`${API}localCases.json`)
+    updateMapMarkers(data)
+
+    localInfected.textContent = data.totalCases
+    localRecovered.textContent = data.recovered
+    localDeaths.textContent = data.deaths
+}
+
+function updateMapMarkers(data){
+
+    data.details.forEach( the => {
+        L.circleMarker([the.lat, the.lng], circleMarkerOpts).addTo(map).bindPopup(`${the.cases} ${the.cases > 1 ? 'casos confirmados' : 'caso confirmado'} en ${the.city}`)
+    })
 }
