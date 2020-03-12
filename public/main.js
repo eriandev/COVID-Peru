d5c8da0885c60d9db0901281d975bec01c059fe9
erianvc <erianvc.dev@gmail.com>
2020-03-11 22:00:58 -0500

    Actualización 11/03/2020 a las 22:00

--- public/main.js
+++ public/main.js
@@ -16,7 +16,7 @@ L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.pn
     attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-L.circleMarker([-12.022867,-77.110848], circleMarkerOpts).addTo(map).bindPopup('10 casos confirmados en Lima')
+L.circleMarker([-12.022867,-77.110848], circleMarkerOpts).addTo(map).bindPopup('11 casos confirmados en Lima')
 L.circleMarker([-9.926930,-76.242220], circleMarkerOpts).addTo(map).bindPopup('2 casos confirmados en Huánuco')
 L.circleMarker([-13.4179319,-76.1396996], circleMarkerOpts).addTo(map).bindPopup('2 casos confirmados en Chincha')
-L.circleMarker([-16.342461,-71.569104], circleMarkerOpts).addTo(map).bindPopup('1 caso confirmado en Arequipa')
+L.circleMarker([-16.342461,-71.569104], circleMarkerOpts).addTo(map).bindPopup('2 casos confirmados en Arequipa')
